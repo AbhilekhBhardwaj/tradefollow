f9b04d69c2c7a24c5180f9e93c67b65812284a37
Abhilekh Bhardwaj <bhardwaj.abhilekh@gmail.com>
2026-06-21 09:26:41 +0530

    fixed build error

--- app/providers.js
+++ app/providers.js
@@ -8,7 +8,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const config = getDefaultConfig({
   appName: 'TradeFollow',
-  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID ?? '',
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID ?? '00000000000000000000000000000000',
   chains: [arbitrum],
   ssr: true,
 })
